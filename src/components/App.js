3c6c07d82a0f239f4c29eee093edf17087c148c5
Lori Henderson <lorim95@gmail.com>
2022-06-07 11:11:45 -0400

    commit tree 3593ae1a1a19c7da8304c716d9f6584cfe50e3b9

--- src/components/App.js
+++ src/components/App.js
@@ -7,12 +7,12 @@ import About from "./About";
 import user from "../data/user";
 console.log(user);
 
-function App() {
+function App(props) {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home color={user.color} name={user.name} city={user.city} />
+      <About bio={user.bio} github={user.links.github} linkedin={user.links.linkedin} /> 
     </div>
   );
 }
